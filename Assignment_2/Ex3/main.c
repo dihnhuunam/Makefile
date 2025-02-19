2d0060050336244a6b5a2f74078d27b60533fe63
dihnhuunam <dihnhuunam@gmail.com>
2025-02-19 22:49:27 +0700

    [ADD]: Assignemnt 2 - Ex3

--- Assignment_2/Ex3/main.c
+++ Assignment_2/Ex3/main.c
@@ -7,26 +7,28 @@ void absoluteValue(uint8_t *data)
     data[0] = (data[0] > data[1]) ? (data[0] - data[1]) : (data[1] - data[0]);
 }
 
-int main(int argc, char **argv)
+int main()
 {
-    if (argc != 3)
+
+    int a, b;
+    while (1)
     {
-        printf("Vui lòng nhập đúng 2 số dương 8 bits!\n");
-        return 1;
+        printf("Nhập vào 2 số (0 - 255): ");
+        int check = scanf("%d %d", &a, &b);
+
+        if (check != 2 || a > 255 || b > 255 || a < 0 || b < 0)
+        {
+            printf("Các số nhập phải nằm trong phạm vi từ 0 đến 255.\n");
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+        break;
     }
 
-    uint8_t a = (uint8_t)atoi(argv[1]);
-    uint8_t b = (uint8_t)atoi(argv[2]);
-    if (a > 255 || b > 255)
-    {
-        printf("Các số nhập phải nằm trong phạm vi từ 0 đến 255.\n");
-        return 1;
-    }
-
-    uint8_t *data;
-    data[0] = a;
-    data[1] = b;
-
+    uint8_t data[2];
+    data[0] = (uint8_t)a;
+    data[1] = (uint8_t)b;
     absoluteValue(data);
     printf("Giá trị tuyệt đối của hiệu 2 số %d và %d: %d\n", a, b, data[0]);
 
